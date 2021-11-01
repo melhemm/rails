33d50a78c2da1b3a8a681cde7f1f0dabc497dba8
melhemm <jamalmulhemm@gmail.com>
2021-11-01 20:59:34 +0300

    -v add

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module TestGuru
   class Application < Rails::Application
+    VERSION = '0.1.0'.freeze
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
