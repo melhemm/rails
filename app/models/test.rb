e951963a93d51328fe160c8cf6d83c96510305b7
melhemm <jamalmulhemm@gmail.com>
2021-11-03 18:44:19 +0300

    class method

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,7 @@
 class Test < ApplicationRecord
+
+  def self.sort_categories(category_title)
+    Test.includes(:category).where(categories: { title: category_title }).order(title: :desc)
+  end
+
 end
