93d77bfabdeb3cb3a76f9fa6fd0b2981e723ea54
melhemm <jamalmulhemm@gmail.com>
2021-11-03 18:51:58 +0300

    data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,30 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-users = User.create([{ name: 'user' }, { name: 'user2' }])
-categories = Category.create([{ title: 'Front-end' }, { title: 'back-end'}])
-tests = Test.create([{title: 'Javascript', level: 1, category_id: categories[0].id}, {title: 'Rails', level: 2, category_id: categories[1].id}, {title: 'Node.js', level: 2, category_id: categories[1].id}])
-questions = Category.create([{ body: 'Example ques1', test_id: tests[0].id }, { body: 'Example ques2', test_id: tests[1].id}, { body: 'Example ques3', test_id: tests[1].id}])
-answers = Category.create([{ body: 'correct answer', correct: true, question_id: questions[0].id }, { body: 'incorrect', question_id: questions[0].id}, { body: 'incorrect', question_id: questions[0].id}])
+users = User.create([
+  { name: 'user' }, 
+  { name: 'user2' }
+])
+
+categories = Category.create([
+  { title: 'Front-end'}, 
+  { title: 'back-end'}
+])
+
+tests = Test.create([
+  {title: 'Javascript', level: 1, category_id: categories[0].id},
+  {title: 'Rails', level: 2, category_id: categories[1].id}, 
+  {title: 'Node.js', level: 2, category_id: categories[1].id}
+])
+
+questions = Category.create([
+  { body: 'Example ques1', test_id: tests[0].id }, 
+  { body: 'Example ques2', test_id: tests[1].id}, 
+  { body: 'Example ques3', test_id: tests[1].id}
+])
+
+answers = Answer.create([
+  { body: 'correct answer', correct: true, question_id: questions[0].id }, 
+  { body: 'incorrect', question_id: questions[0].id}, 
+  { body: 'incorrect', question_id: questions[0].id}
+])
