ceb409ef843c97d591df254b12a0859ca7795bfc
melhemm <56795250+melhemm@users.noreply.github.com>
2021-11-06 22:19:29 +0300

    Revert "Rails part 4"

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,2 @@
 class Test < ApplicationRecord
-
-  def self.sort_categories(category_title)
-    Test.includes(:category).where(categories: { title: category_title }).order(title: :desc)
-  end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,2 @@
 class User < ApplicationRecord
-  def user_level(level)
-    Test.where(level: level)
-  end
 end
