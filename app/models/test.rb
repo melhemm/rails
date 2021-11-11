016a0eeb91d975be837c9d383117e899cb8ba703
melhemm <jamalmulhemm@gmail.com>
2021-11-11 22:30:36 +0300

    done some improvements

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,6 +3,8 @@ class Test < ApplicationRecord
 
   def self.sort_categories(category)
     joins("JOIN categories ON tests.category_id = categories.id")
-    .where(categories: {title: category}).order(title: :desc).pluck(:title)
+     .where(categories: {title: category})
+     .order(title: :desc)
+     .pluck(:title)
   end
 end
