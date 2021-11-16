a096fd6e010b97514ae90662d107582b40a4a5d9
melhemm <jamalmulhemm@gmail.com>
2021-11-16 23:36:13 +0300

    fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :user_tests, dependent: :destroy
   has_many :tests, through: :user_tests
+  has_many :tests_as_auther, class_name: "Test", foreign_key: "author_id"
   
   def tests_in_level(level)
     tests.where(level: level)
