240c4c08ccc6c45d16fe6662fe2200c85de79965
melhemm <jamalmulhemm@gmail.com>
2021-11-17 22:45:03 +0300

    validation and scope methods

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,16 @@
 class Answer < ApplicationRecord
+
+  MAX_ANSWERS_VALIDATION = 4
+
   belongs_to :question
+
+  scope :correct_answer, -> { where(correct: true)}
+
+  validates :validate_answers, on: :create
+
+  def validate_answers
+    errors.add(:answer) 
+    if question.answers.size >= MAX_ANSWERS_VALIDATION
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :user_tests, dependent: :destroy
   has_many :tests, through: :user_tests
   has_many :tests_as_auther, class_name: "Test", foreign_key: "author_id"
+
+  validates :email, presence: true
   
   def tests_in_level(level)
     tests.where(level: level)
