7338fc45d5a22aca7d74b1fa95b545d5dd73e600
melhemm <jamalmulhemm@gmail.com>
2021-12-05 22:34:34 +0300

    rails transaction

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,16 +1,16 @@
 class Answer < ApplicationRecord
-  MAX_ANSWERS_VALIDATION = 4
+  # MAX_ANSWERS_VALIDATION = 4
 
   belongs_to :question
 
   scope :correct, -> { where(correct: true)}
 
-  validate :validate_answers
+  # validate :validate_answers
 
   private
 
-  def validate_answers
-    errors.add(:base, "Answers must be from 1 to 4") if question.answers.count >= MAX_ANSWERS_VALIDATION
-  end
+  # def validate_answers
+  #   errors.add(:base, "Answers must be from 1 to 4") if question.answers.count >= MAX_ANSWERS_VALIDATION
+  # end
 
 end
