833f9042514e015ebf80e5d3a01306d2c5acf0a7
melhemm <jamalmulhemm@gmail.com>
2021-12-20 23:19:48 +0300

    some improvements and fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,10 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(user)
-    flash[:notice] = "Hello, #{user.first_name}"
-    
     if current_user.is_a?(Admin)
       admin_tests_path
     else
-      stored_location_for(user)
+      super
     end
   end
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,8 @@
-class SessionsController < ApplicationController
-
-  def new; end
+class SessionsController < Devise::SessionsController
 
   def create
-    user = User.find_by(email: params[:email])
-
-    if user&.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect_to cookies.delete(:redirect_url) || root_path
-    else
-      flash.now[:alert] = 'Check email and password'
-      render :new
-    end
+    super
+    flash[:notice] = "Hello, #{current_user.first_name}"
   end
 
   def destroy
