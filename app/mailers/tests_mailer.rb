cec109f3454319aaef874d42722c3a206749ecbb
melhemm <jamalmulhemm@gmail.com>
2021-12-22 18:24:26 +0300

    localizing

--- app/mailers/tests_mailer.rb
+++ app/mailers/tests_mailer.rb
@@ -4,6 +4,6 @@ class TestsMailer < ApplicationMailer
     @user = test_passage.user
     @test = test_passage.test
 
-    mail to: @user.email, subject: 'You have just completed the TestGuru!' 
+    mail to: @user.email 
   end
 end
---
+++ app/views/tests_mailer/completed_test.ru.html.rb
@@ -0,0 +1,8 @@
+<h1> результаты! </h1>
+
+<p>
+  Поздравления <b> <%= @test.title %> <b>
+  More Тесты
+
+  <%= link_to 'more', tests_url %>
+</p>
