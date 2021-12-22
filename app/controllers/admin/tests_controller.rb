c93bd66f9e7865257e0fe6e0f78b717c8f80e094
melhemm <jamalmulhemm@gmail.com>
2021-12-22 17:17:38 +0300

    Internationalization

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -16,7 +16,7 @@ class Admin::TestsController < Admin::BaseController
     @test = current_user.tests.new(test_params)
 
     if @test.save
-      redirect_to admin_test_path(@test)
+      redirect_to admin_test_path(@test), notice: t('.success')
     else
       render :new
     end
--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,6 @@ module TestGuru
     # in config/environments, which are processed later.
     #
     config.time_zone = 'Moscow'
-    config.i18n.available_locales = :ru
     config.i18n.default_locale = :ru
     # config.eager_load_paths << Rails.root.join("extras")
   end
