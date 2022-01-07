9e6777dbb3b29e323cbd611321e5694eac90e45f
melhemm <jamalmulhemm@gmail.com>
2022-01-07 22:01:46 +0300

    improvements and progress bar added

--- app/javascript/utilities/form_inline.js
+++ app/javascript/utilities/form_inline.js
@@ -22,17 +22,18 @@ function formInlineLinkHandler(e) {
 }
 
 function formInlineHandler(testId) {
-  const link = document.querySelector('.form-inline-link[data-test-id="' + testId + '"]')
-  const testTitle = document.querySelector('.test-title[data-test-id="' + testId + '"]')
-  const formInline = document.querySelector('.form-inline[data-test-id="' + testId + '"]')
+  const dataAtrr = `[data-test-id="${testId}"]`
+  const link = document.querySelector(`.form-inline-link${dataAtrr}`)
+  const testTitle = document.querySelector(`.test-title${dataAtrr}`)
+  const formInline = document.querySelector(`.form-inline${dataAtrr}`)
 
   if (formInline.classList.contains('hide')) {
     testTitle.classList.add('hide')
     formInline.classList.remove('hide')
-    link.textContent = "Cancel"
+    link.textContent = link.dataset.cancel
   } else {
     testTitle.classList.remove('hide')
     formInline.classList.add('hide')
-    link.textContent = "Edit"
+    link.textContent = link.dataset.edit
   }
 }
