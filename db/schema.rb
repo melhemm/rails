b5fdaa4cb4eb5125d4ea5867a17409ef0a1516b3
melhemm <jamalmulhemm@gmail.com>
2022-01-08 13:12:27 +0300

    preparation for production

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_12_27_124205) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
     t.integer "question_id", null: false
