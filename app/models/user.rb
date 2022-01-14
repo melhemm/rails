72d0a873154ec4a5a5b30cfa7c33fdc734e1d9d4
melhemm <jamalmulhemm@gmail.com>
2022-01-14 23:19:23 +0300

    badge - user_badge model and migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   has_many :tests, through: :test_passages
   has_many :tests_as_auther, class_name: "Test", foreign_key: "author_id", dependent: :destroy
   has_many :feedbacks, dependent: :destroy
+  has_many :user_badges, dependent: :destroy
+  has_many :badges, through: :user_badges
 
   validates :email, uniqueness: true, format: {with: /[a-zA-Z0-9_\-\.]{0,}([.]?[a-zA-Z0-9]{1,})[@](gmail.com|hotmail.com|yahoo.com|yandex.com|yandex.ru|mail.ru)/}
   
